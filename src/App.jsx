c22340ce8e28a0d323dd7028d28da761c2e6230e
Jashwant Das <jashwantswami367@gmail.com>
2026-05-17 22:26:16 +0530

    Medie

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import NavBar from "./components/NavBar";
-import { Routes,Route} from "react-router-dom";
+import { Routes,Route,Link} from "react-router-dom";
 import Home from "./pages/Home";
 import CollectionPage from "./pages/CollectionPages";
 import { ToastContainer } from "react-toastify";
 export default function App() {
   return (
     <div className=" text-white w-full bg-gray-900">
-       <NavBar/>
+       <Link to={'/'}><NavBar/></Link>
       <Routes>
         <Route path="/"  element={<Home/>}/>
         <Route path="collection" element={<CollectionPage/>}/>
